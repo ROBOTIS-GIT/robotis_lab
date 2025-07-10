e7b266f7adf5d992b2b3373d12812be792b803b8
kimtaehyeong99 <kth@robotis.com>
2025-07-10 16:00:00 +0900

    omy reach task parameter tuning
    Signed-off-by: kimtaehyeong99 <kth@robotis.com>

--- source/robotis_lab/robotis_lab/tasks/manager_based/OMY/reach/reach_env_cfg.py
+++ source/robotis_lab/robotis_lab/tasks/manager_based/OMY/reach/reach_env_cfg.py
@@ -167,7 +167,7 @@ class RewardsCfg:
     action_rate = RewTerm(func=mdp.action_rate_l2, weight=-0.0001)
     joint_vel = RewTerm(
         func=mdp.joint_vel_l2,
-        weight=-0.001,
+        weight=-0.0001,
         params={"asset_cfg": SceneEntityCfg("robot")},
     )
 
@@ -184,6 +184,9 @@ class CurriculumCfg:
 
     action_rate = CurrTerm(
         func=mdp.modify_reward_weight, params={"term_name": "action_rate", "weight": -0.005, "num_steps": 4500}
+    )
+    joint_vel = CurrTerm(
+        func=mdp.modify_reward_weight, params={"term_name": "joint_vel", "weight": -0.001, "num_steps": 4500}
     )
 
 ##
