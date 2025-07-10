ba742fdf72033821d8bc953aa7de2d3cf88d4153
kimtaehyeong99 <kth@robotis.com>
2025-07-10 18:11:45 +0900

    reach task parameter tuning
    Signed-off-by: kimtaehyeong99 <kth@robotis.com>

--- scripts/sim2real/OMY/reach/reach_env_cfg.py
+++ scripts/sim2real/OMY/reach/reach_env_cfg.py
@@ -15,7 +15,7 @@ class ReachEnvConfig:
         self.send_command_interval = 3.0 # seconds
 
         self.joint_state_topic = "/arm_controller/controller_state"
-        self.joint_trajectory_topic = "/leader/joint_trajectory"
+        self.joint_trajectory_topic = "/arm_controller/joint_trajectory"
 
         repo_root = Path(__file__).resolve().parents[4]
         self.policy_path = repo_root / "logs/rsl_rl/reach_omy" / model_dir / "exported/policy.pt"
